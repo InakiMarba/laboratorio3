ceaeece3b12320b5e24bf27dc898a63d12c1bfe1
InakiMarba <imarba@inscaparrella.cat>
2023-12-03 09:37:29 +0100

    NuevosTests

--- tests/HeapTest.java
+++ tests/HeapTest.java
@@ -54,5 +54,52 @@ public class HeapTest {
         assertEquals(1, heapQueue.size());
     }
 
+    @Test
+    void testGeneral() {
+        HeapQueue<Integer, String> heap = new HeapQueue<>();
+
+        // Agregar elementos
+        heap.add(5, "Five");
+        heap.add(2, "Two");
+        heap.add(8, "Eight");
+        heap.add(1, "One");
+
+        // Verificar tamaño y acceso al elemento de mayor prioridad
+        assertEquals(4, heap.size());
+        assertEquals("Eight", heap.element());
+
+        // Eliminar el elemento de mayor prioridad
+        String removed = heap.remove();
+        assertEquals("Eight", removed);
+        assertEquals(3, heap.size());
+        assertEquals("Five", heap.element());
+
+        // Agregar más elementos
+        heap.add(10, "Ten");
+        heap.add(3, "Three");
+
+        // Verificar el nuevo elemento de mayor prioridad después de agregar más elementos
+        assertEquals(5, heap.size());
+        assertEquals("Ten", heap.element());
+
+        // Eliminar todos los elementos restantes
+        heap.remove();
+        heap.remove();
+        heap.remove();
+        heap.remove();
+        heap.remove();
+
+        // Verificar que la estructura esté vacía después de eliminar todos los elementos
+        assertEquals(0, heap.size());
+        assertThrows(NoSuchElementException.class, heap::element);
+        }
+
+    @Test
+    void testRemoveOnEmptyHeapShouldThrowException() {
+        HeapQueue<Integer, String> heap = new HeapQueue<>();
+
+        // Verificar que lanzar NoSuchElementException al intentar eliminar de un heap vacío
+        assertThrows(NoSuchElementException.class, heap::remove);
+    }
 
 }
